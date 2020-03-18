692ed206c2fb9d7885f1731493cb2f285163567d
nishant <nish54@gmail.com>
2020-03-17 22:45:33 -0500

    working discord bot, added gitignore

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 a discord bot made in python
 
 test commit
+
+## Dependencies
+* discord.py - https://discordpy.readthedocs.io/en/latest/intro.html#installing
---
+++ app/waddle-bot.py
@@ -0,0 +1,20 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run('Njg5Njc2MTk3MTg1MTkxOTM4.XnGV-g.HaasCsYFrY87F8i9OYJfOYmCjbw')
